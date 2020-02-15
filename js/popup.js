084e1eaccd74a0224541874280a89052ec7e30b9
Yuriy <yuriy.marin@gmail.com>
2020-02-15 23:59:08 +0300

    В js/popup.js добавлена функция вывода окна с сообщение об успехе
    Обе функции (success() и submit()) теперь работают с опциональным
    указываен параметра message. Если вызывать их без него, покажется
    сообщение по умолчанию, если ним — сообщение из параметра.
    
    Также исправлена баг с повторным вызовом одного и того же сообщения
    — раньше элементы не клонировались, но удалялись, поэтому вызов
    второго сообщения в сессии вызывал ошибку. Поправлено.

--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,43 @@
 'use strict';
 
 window.popup = (function () {
-  function error(message) {
-    var errorElement = document.querySelector('#error').content.querySelector('.error');
-    errorElement.querySelector('.error__message').textContent = message;
+  var error = function (message) {
+    var errorTemplate = document.querySelector('#error').content.querySelector('.error');
+    var errorElement = errorTemplate.cloneNode(true);
+    if (message) {
+      errorElement.querySelector('.error__message').textContent = message;
+    }
+    
     document.querySelector('main').appendChild(errorElement);
     errorElement.querySelector('.error__button').addEventListener('click', function () {
       errorElement.remove();
     });
   }
+  
+  var success = function (message) {
+    var successTemplate = document.querySelector('#success').content.querySelector('.success');
+    var successElement = successTemplate.cloneNode(true);
+    var closeByEscHandler = function (evt) {
+      if (window.util.isEscPressed(evt)) {
+        successElement.remove();
+        document.removeEventListener('keydown', closeByEscHandler);
+      }
+    }
+    
+    if (message) {
+      successElement.querySelector('.success__message').textContent = message;
+    }
+    
+    document.querySelector('main').appendChild(successElement);
+    successElement.addEventListener('click', function () {
+      successElement.remove();
+    });
+    
+    document.addEventListener('keydown', closeByEscHandler);
+  }
 
   return {
-    error: error
+    error: error,
+    success: success
   };
 })();
