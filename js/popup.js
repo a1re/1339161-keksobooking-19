6fe683d786b3c9199b6b6d2928f8956be5532cf1
Yuriy <yuriy.marin@gmail.com>
2020-02-16 00:07:48 +0300

    Оптимизирован код в js/popup.js и добавлено закрытие ошибки по Esc
    Закрытие окна по Esc вынесено в отдельную функцию setCloseByEsc()
    во имя DRY.

--- js/popup.js
+++ js/popup.js
@@ -1,40 +1,48 @@
 'use strict';
 
 window.popup = (function () {
+  var setCloseByEsc = function (element) {
+    var closeByEscHandler = function (evt) {
+      if (window.util.isEscPressed(evt)) {
+        element.remove();
+        document.removeEventListener('keydown', closeByEscHandler);
+      }
+    };
+
+    document.addEventListener('keydown', closeByEscHandler);
+  };
+
   var error = function (message) {
     var errorTemplate = document.querySelector('#error').content.querySelector('.error');
     var errorElement = errorTemplate.cloneNode(true);
+
     if (message) {
       errorElement.querySelector('.error__message').textContent = message;
     }
-    
+
     document.querySelector('main').appendChild(errorElement);
     errorElement.querySelector('.error__button').addEventListener('click', function () {
       errorElement.remove();
     });
-  }
-  
+
+    setCloseByEsc(errorElement);
+  };
+
   var success = function (message) {
     var successTemplate = document.querySelector('#success').content.querySelector('.success');
     var successElement = successTemplate.cloneNode(true);
-    var closeByEscHandler = function (evt) {
-      if (window.util.isEscPressed(evt)) {
-        successElement.remove();
-        document.removeEventListener('keydown', closeByEscHandler);
-      }
-    }
-    
+
     if (message) {
       successElement.querySelector('.success__message').textContent = message;
     }
-    
+
     document.querySelector('main').appendChild(successElement);
     successElement.addEventListener('click', function () {
       successElement.remove();
     });
-    
-    document.addEventListener('keydown', closeByEscHandler);
-  }
+
+    setCloseByEsc(successElement);
+  };
 
   return {
     error: error,
