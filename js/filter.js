13e4443fabdeabd1386d1b765f41a911f8a71069
Yuriy <yuriy.marin@gmail.com>
2020-02-19 02:08:38 +0300

    Добавлены функции фильтрации объявлений
    Общий механизм такой: в начале модуля перечисляется список
    фильтров, работающих через чекбокс и через селектор. Для
    фильтров-чекобксов указан просто список ключевых слов, для
    фильтров-селекторов дополнительно указана функция-сравнение,
    которая принимает на вход текущее значение поля и объект пина
    со всеми данными. Если она возвращает true, то пин остается
    в подборке, если false — то он исчезает. Фильтры добавляются при
    изменении своих полей через функцию addFilter( ) — либо, наоборот,
    удаляются через removeFilter( ), если фильтр принял значение
    по умолчанию.
    
    При каждом изменении фильтра применяется функция applyFilters( ),
    которая пробегает по общему списку пинов и применяет к ним
    добавленные фильтры. При этом пробегать совсем по всем фильтрам
    не обязательно — как только попался первый, вернувший false,
    перебор останавливается и пин исчезает из выборки. Если ни один
    фильтр не вернул false, то пин остается в подборке.

--- js/filter.js
+++ js/filter.js
@@ -1,9 +1,52 @@
 'use strict';
 
 window.filter = (function () {
+  var CANCEL_FILTER_SELECTOR_VALUE = 'any';
+
   var updatePins = null;
-  var housingTypeElement = document.querySelector('#housing-type');
-  var ANY_HOUSUNG_TYPE = 'any';
+  var PriceBoundary = {
+    LOW: 10000,
+    HIGH: 50000
+  };
+  var checkboxFilters = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+  var selectorFilters = [
+    {
+      name: 'housing-type',
+      filter: function (selectedValue, pinData) {
+        return selectedValue === pinData.offer.type;
+      }
+    },
+    {
+      name: 'housing-price',
+      filter: function (selectedValue, pinData) {
+        switch (selectedValue) {
+          case 'middle':
+            return (pinData.offer.price >= PriceBoundary.LOW && pinData.offer.price <= PriceBoundary.HIGH);
+          case 'low':
+            return (pinData.offer.price < PriceBoundary.LOW);
+          case 'high':
+            return (pinData.offer.price > PriceBoundary.HIGH);
+          default:
+            return true;
+        }
+      }
+    },
+    {
+      name: 'housing-rooms',
+      filter: function (selectedValue, pinData) {
+        return parseInt(selectedValue, 10) === parseInt(pinData.offer.rooms, 10);
+      }
+    },
+    {
+      name: 'housing-guests',
+      filter: function (selectedValue, pinData) {
+        return parseInt(selectedValue, 10) === parseInt(pinData.offer.guests, 10);
+      }
+    },
+  ];
+  var filtersForm = document.querySelector('.map__filters-container');
+
+  var filters = [];
 
   var deactivate = function () {
     var fieldsList = document.querySelectorAll('.map__filters select, .map__filters fieldset');
@@ -23,18 +66,63 @@ window.filter = (function () {
     updatePins = pinHandler;
   };
 
-  var housingTypeChangeHandler = function (housingTypeEvt) {
-    var selectedType = housingTypeEvt.currentTarget.value;
-    var filteredPins = window.data.pins.filter(function (pinData) {
-      return selectedType === pinData.offer.type || selectedType === ANY_HOUSUNG_TYPE;
+  var addFilter = function (name, selectedValue, filter) {
+    removeFilter(name);
+    filters.push({
+      name: name,
+      selectedValue: selectedValue,
+      executeFilter: filter
     });
+  };
 
+  var removeFilter = function (name) {
+    for (var i = 0; i < filters.length; i++) {
+      if (filters[i].name === name) {
+        filters.splice(i, 1);
+        return;
+      }
+    }
+  };
+
+  var applyFilters = function () {
+    var filteredPins = window.data.pins.filter(function (pinData) {
+      for (var i = 0; i < filters.length; i++) {
+        if (!filters[i].executeFilter(filters[i].selectedValue, pinData)) {
+          return false;
+        }
+      }
+      return true;
+    });
     if (typeof updatePins === 'function') {
       updatePins(filteredPins);
     }
   };
 
-  housingTypeElement.addEventListener('change', housingTypeChangeHandler);
+  checkboxFilters.forEach(function (featureName) {
+    var checkboxElement = filtersForm.querySelector('#filter-' + featureName);
+    checkboxElement.addEventListener('change', function (evt) {
+      if (evt.currentTarget.checked) {
+        addFilter(featureName, featureName, function (selectedValue, pinData) {
+          return (pinData.offer.features.indexOf(selectedValue) >= 0);
+        });
+      } else {
+        removeFilter(featureName);
+      }
+      applyFilters();
+    });
+  });
+
+  selectorFilters.forEach(function (feature) {
+    var selectorElement = filtersForm.querySelector('#' + feature.name);
+    selectorElement.addEventListener('change', function (evt) {
+      if (evt.currentTarget.value !== CANCEL_FILTER_SELECTOR_VALUE) {
+        addFilter(feature.name, evt.currentTarget.value, feature.filter);
+      } else {
+        removeFilter(feature.name);
+      }
+      applyFilters();
+    });
+  });
 
   return {
     deactivate: deactivate,
