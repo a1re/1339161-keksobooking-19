b4c30c6c6106b57176a35f8fe2469fedce055f40
Yuriy <yuriy.marin@gmail.com>
2020-03-06 18:38:03 +0300

    Добавлены комментарии к модулям js/form-field.js и js/form.js

--- js/form-field.js
+++ js/form-field.js
@@ -1,18 +1,35 @@
 'use strict';
 
+/**
+ * Абстрактный объект для работы с полями формы объявления. Создается через
+ * конструктор Field (снаружи модуля — window.FormField) и настраивается
+ * при помощи методов, определенных через прототип.
+ */
+
 window.FormField = (function () {
   var FILE_EXTENSIONS = ['jpg', 'jpeg', 'png'];
   var INSTANT_CHECK_TIMEOUT = 5000;
-  
+
+  /**
+   * Конструктор класса Field
+   *
+   * @constructor
+   * @param {string} selector - DOM-селектор поля, для которого создается объект
+   */
   var Field = function (selector) {
     var self = this;
     self.element = document.querySelector(selector);
     self.isValid = true;
-    
-    if (self.element.type === "text" || self.element.type === "number") {
+
+    // Если поле текстовое или цифровое, то валидация запускаются при нажатии
+    // клавиш с небольшим таймаутом, чтобы не подсвечивать поле красным цветом
+    // раньше времени (пока пользователь еще заполняет), а также при снятии
+    // фокуса. Если поле другого типа, то валидация происходит при событии
+    // change
+    if (self.element.type === 'text' || self.element.type === 'number') {
       self.element.addEventListener('keyup', function () {
         if (self.validate(true)) {
-          self.setValid(); 
+          self.setValid();
         } else {
           window.util.debounce(self.validate.bind(self), INSTANT_CHECK_TIMEOUT)();
         }
@@ -27,10 +44,29 @@ window.FormField = (function () {
     }
   };
 
+  /**
+   * Добавляет функцию валидации к полю
+   *
+   * @param {function} procedure - функция, валидирующая значение поля. Доступ
+   *                               к свойствам поля осуществляется через this.
+   *                               Функция должна возвращать логическое (bool)
+   *                               значение: если True, то значение прошло
+   *                               проверку, если False — то нет
+   * @return {undefined}
+   */
   Field.prototype.setValidation = function (procedure) {
     this.validationProcedure = procedure.bind(this);
   };
 
+  /**
+   * Проверяет значение поля. Если задана функция валидацции через
+   * setValidation, то запускается она, если нет, то статус берется
+   * из браузерной проверки.
+   *
+   * @param {bool} [getStatusOnly] - если True, то полю не устанавливается сам
+   *                                 статус, а только возвращается из метода
+   * @return {bool} True если значение прошло проверку, False если нет
+   */
   Field.prototype.validate = function (getStatusOnly) {
     if (typeof this.validationProcedure === 'function') {
       this.isValid = this.validationProcedure();
@@ -43,13 +79,27 @@ window.FormField = (function () {
     return this.isValid ? this.setValid() : this.setInvalid();
   };
 
+  /**
+   * Отмечает поле, как валидное
+   *
+   * @return {bool} True
+   */
   Field.prototype.setValid = function () {
+    this.isValid = true;
     this.element.classList.remove('invalid');
     this.element.setCustomValidity('');
     return true;
   };
 
+  /**
+   * Отмечает поле, как невалидное
+   *
+   * @param {string} message — сообщение о статусе для пользователя (выводится
+   *                           срдствами валидации браузера.
+   * @return {bool} False
+   */
   Field.prototype.setInvalid = function (message) {
+    this.isValid = false;
     this.element.classList.add('invalid');
     if (message) {
       this.element.setCustomValidity(message);
@@ -57,25 +107,59 @@ window.FormField = (function () {
     return false;
   };
 
+  /**
+   * Отключает поле, устанавливая ему атрибут disabled
+   *
+   * @return {undefined}
+   */
   Field.prototype.disable = function () {
     this.element.disabled = true;
   };
 
+  /**
+   * Включает поле, отключая ему атрибут disabled
+   *
+   * @return {undefined}
+   */
   Field.prototype.enable = function () {
     this.element.disabled = false;
   };
 
+  /**
+   * Добавляет для пользователя функцию, которая срабатывает при сбросе значений
+   * всей формы
+   *
+   * @param {function} procedure — функция, срабатывающая при сбросе значений
+   *                               формы (reset). Доступ к свойствам поля
+   *                               осуществляется через this.
+   * @return {undefined}
+   */
   Field.prototype.setReset = function (procedure) {
     this.resetProcedure = procedure.bind(this);
   };
 
+  /**
+   * Сбрасывает значение поля и помечает его как валидное. Если для него
+   * установлена функция через setReset, то она срабатывает.
+   *
+   * @return {undefined}
+   */
   Field.prototype.reset = function () {
     if (typeof this.resetProcedure === 'function') {
       this.resetProcedure();
     }
     this.setValid();
   };
-  
+
+  /**
+   * Добавляет элемент для превью для поля с загрузкой изображения и
+   * отрабатывает процедуру выбора файла. При сбросе значений, восставливает
+   * содержание элемента, которое было на момент установки.
+   *
+   * @param {string} selector — DOM-селектор поля, в которое будет помещаться
+   *                            изображение-превью.
+   * @return {undefined}
+   */
   Field.prototype.setUploadImagePreview = function (selector) {
     var previewHolder = document.querySelector(selector);
     var children = previewHolder.querySelectorAll('*');
@@ -84,7 +168,7 @@ window.FormField = (function () {
       var child = childElement.cloneNode(true);
       defaultPreview.appendChild(child);
     });
-     
+
     var uploadPreviewImageHandler = function () {
       var file = this.element.files[0];
       var fileName = file.name.toLowerCase();
@@ -107,14 +191,14 @@ window.FormField = (function () {
         reader.readAsDataURL(file);
       }
     };
-     
+
     this.element.addEventListener('change', uploadPreviewImageHandler.bind(this));
-     
+
     this.setReset(function () {
       window.util.removeElements('*', previewHolder);
       previewHolder.appendChild(defaultPreview.cloneNode(true));
     });
-  }
-  
+  };
+
   return Field;
 })();
--- js/form.js
+++ js/form.js
@@ -1,9 +1,16 @@
 'use strict';
 
+/**
+ * Модуль управления полями формы добавления объявления. Поля управляются
+ * с помощью объекта window.FormField и его методов.
+ */
+
 (function () {
-  var form = document.querySelector('.ad-form');  
+  var form = document.querySelector('.ad-form');
   var formSubmitButton = form.querySelector('.ad-form__submit');
-  
+
+  // Создаем словарь со списком объектов, где ключ — название поля. Поля
+  // создаютс по DOM-селектору.
   var fields = {
     'title': new window.FormField('#title'),
     'type': new window.FormField('#type'),
@@ -12,11 +19,15 @@
     'capacity': new window.FormField('#capacity'),
     'timein': new window.FormField('#timein'),
     'timeout': new window.FormField('#timeout'),
-    'timeout': new window.FormField('#timeout'),
     'avatar': new window.FormField('#avatar'),
     'images': new window.FormField('#images')
   };
-  
+
+  /**
+   * Функция проверки соответствия значений полей roomNumber и capacity
+   *
+   * @return {bool} True, если значения корректны, False если нет
+   */
   var checkRoomsAndCapacity = function () {
     var roomNumber = parseInt(fields['roomNumber'].element.value, 10);
     var capacity = parseInt(fields['capacity'].element.value, 10);
@@ -29,6 +40,9 @@
     return false;
   };
 
+  // Добавление валидации к полю "Тип жилья". Функция всегда возвращает true,
+  // но при смене значения обновляет минимальное значение и плейсхолдер для
+  // поля цены
   fields['type'].setValidation(function () {
     var housingType = window.page.housingTypeMap[this.element.value];
     fields['price'].element.min = housingType.minPrice;
@@ -37,33 +51,50 @@
     return true;
   });
 
+  // Добавление валидации к полю "Количество комнат". При изменениии значения
+  // проверяет соответствие полю "Количество мест" и сразу отмечает поле
+  // невалидным, если оно не соответствует
   fields['roomNumber'].setValidation(function () {
     if (checkRoomsAndCapacity()) {
       return fields['capacity'].setValid();
     }
-    return this.setInvalid(window.page.Message.CAPACITY_ERROR)
+    return this.setInvalid(window.page.Message.CAPACITY_ERROR);
   });
 
+  // Добавление валидации к полю "Количество мест". При изменениии значения
+  // проверяет соответствие полю "Количество комнат" и сразу отмечает поле
+  // невалидным, если оно не соответствует
   fields['capacity'].setValidation(function () {
     if (checkRoomsAndCapacity()) {
       return fields['roomNumber'].setValid();
     }
-    return this.setInvalid(window.page.Message.CAPACITY_ERROR)
+    return this.setInvalid(window.page.Message.CAPACITY_ERROR);
   });
 
+  // Добавление валидации к полю "Время заезда". Функция всегда возвращает true,
+  // но при изменении обновляет значение связанного с ним поля "Время выезда"
   fields['timein'].setValidation(function () {
     fields['timeout'].element.value = fields['timein'].element.value;
     return true;
   });
 
+  // Добавление валидации к полю "Время выезда". Функция всегда возвращает true,
+  // но при изменении обновляет значение связанного с ним поля "Время заезда"
   fields['timeout'].setValidation(function () {
     fields['timein'].element.value = fields['timeout'].element.value;
     return true;
   });
-  
+
+  // Добавления элементов для превью для полей "Ваша фотография" и "Фотография
+  // жилья"
   fields['avatar'].setUploadImagePreview('.ad-form-header__preview');
   fields['images'].setUploadImagePreview('.ad-form__photo');
 
+  // Добавления процедуры сброса значений для поля "Цена за ночь", т.к. при
+  // смене значения поля "Тип жилья" в "Цене за ночь" обновляются и атрибуты
+  // тоже и при обычном сбросе значения возвращаются именно к ним. Сброс
+  // срабатывает по минимальному таймауту, чтобы получить значения уже после
+  // сброса поля "Тип жилья"
   fields['price'].setReset(function () {
     setTimeout(function () {
       var housingType = window.page.housingTypeMap[fields['type'].element.value];
@@ -72,6 +103,12 @@
     }, 1);
   });
 
+  /**
+   * Функция которая вызывается при успешном сохранении данных через
+   * XMLHttpRequest
+   *
+   * @return {undefined}
+   */
   var submitLoadHandler = function () {
     formSubmitButton.textContent = window.page.Message.SAVE_FORM_DEFAULT;
     window.page.showSuccess();
@@ -79,35 +116,51 @@
     form.reset();
   };
 
+  /**
+   * Функция которая вызывается при ошибке при сохранении данных через
+   * XMLHttpRequest
+   *
+   * @param {string} message - Сообщенеи об ошибке
+   * @return {undefined}
+   */
   var submitErrorHandler = function (message) {
     formSubmitButton.textContent = window.page.Message.SAVE_FORM_DEFAULT;
     window.page.showError(message);
   };
-  
-  formSubmitButton.addEventListener('click', function (evt) {
+
+  // Проверка полей через метод объекта Field при сабмите формы. Привязан
+  // по клику т.к. на сабмите всегда сначала срабатывает браузерная валидация,
+  // а она не дает дружелюбно подсветить поля
+  formSubmitButton.addEventListener('click', function () {
     Object.keys(fields).forEach(function (name) {
       fields[name].validate();
     });
   });
-  
+
+  // Сабмит формы через XMLHttpRequest
   form.addEventListener('submit', function (evt) {
     evt.preventDefault();
     formSubmitButton.textContent = window.page.Message.SAVE_FORM_LOADING;
     window.data.save(new FormData(form), submitLoadHandler, submitErrorHandler);
   });
-  
-  form.addEventListener('reset', function (evt) {
+
+  // Сброс значений поля — для всех полей помимо браузерного резета, срабатывает
+  // еще резет из объекта Field
+  form.addEventListener('reset', function () {
     Object.keys(fields).forEach(function (name) {
       fields[name].reset();
     });
     window.page.deactivate();
   });
 
+  // Добавление активации полей при активации страницы
   window.page.addActivationProcedure(function () {
     form.classList.remove('ad-form--disabled');
     Object.keys(fields).forEach(function (name) {
       fields[name].enable();
     });
+    // Некоторые поля деактивируются вручную по селектору т.к. для них
+    // не создается объект Field
     document.querySelector('#description').disabled = false;
     document.querySelector('.ad-form .features').disabled = false;
     document.querySelector('#address').disabled = false;
@@ -115,11 +168,14 @@
     document.querySelector('.ad-form__reset').disabled = false;
   });
 
+  // Добавление деактивации полей при деактивации страницы
   window.page.addDeactivationProcedure(function () {
     form.classList.add('ad-form--disabled');
     Object.keys(fields).forEach(function (name) {
       fields[name].disable();
     });
+    // Некоторые поля деактивируются вручную по селектору т.к. для них
+    // не создается объект Field
     document.querySelector('#description').disabled = true;
     document.querySelector('.ad-form .features').disabled = true;
     document.querySelector('#address').disabled = true;
