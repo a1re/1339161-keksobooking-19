9da9db06b84edbc9737bea14064a730d495bf925
Yuriy <yuriy.marin@gmail.com>
2020-03-08 02:42:03 +0300

    Добавлены комментарии к модулю js/map.js
    Также деактивация страницы вынесена в отдельный скрипт в самом index.html

--- js/map.js
+++ js/map.js
@@ -1,11 +1,25 @@
 'use strict';
 
+/**
+ * Модуль управления картой и пинами на ней.
+ *
+ * Главный пин, который определяет адрес для пользовательского объявления,
+ * называется "Поинтер" (Pointer), чтобы не было путаницы между ним и остальными
+ * пинами.
+ */
+
 window.map = (function () {
   var pointer = document.querySelector('.map__pin--main');
   var pinsHolder = document.querySelector('.map__pins');
   var pointerDefaultLeft = parseInt(pointer.style.left, 10);
   var pointerDefaultTop = parseInt(pointer.style.top, 10);
+  var addressField = document.querySelector('#address');
 
+  /**
+   * Функция обновления координат поинтера в поле "Адрес"
+   *
+   * @return {undefined}
+   */
   var updateAddress = function () {
     var coords = {
       x: parseInt(pointer.style.left, 10),
@@ -20,11 +34,16 @@ window.map = (function () {
     } else {
       coords.y += Math.round(window.page.PointerSize.DIAMETER / 2);
     }
-    
-    var addressField = document.querySelector('#address');
+
     addressField.value = coords.x + ', ' + coords.y;
   };
 
+  /**
+   * Обработчик перемещения поинтера по карте
+   *
+   * @param {Event} evt — Объект события, полученное через обработчик события
+   * @return {undefined}
+   */
   var updatePointerPosition = function (evt) {
     evt.preventDefault();
 
@@ -61,9 +80,9 @@ window.map = (function () {
                                    window.page.PointerSize.DIAMETER -
                                    window.page.PointerSize.PILLAR;
       var adjustedLeftBoundary = window.page.Boundary.LEFT -
-                                  Math.round(window.page.PointerSize.DIAMETER/2);
+                                  Math.round(window.page.PointerSize.DIAMETER / 2);
       var adjustedRightBoundary = window.page.Boundary.RIGHT -
-                                  Math.round(window.page.PointerSize.DIAMETER/2);
+                                  Math.round(window.page.PointerSize.DIAMETER / 2);
 
       if (pointer.offsetLeft - delta.x <= adjustedLeftBoundary) {
         pointer.style.left = adjustedLeftBoundary + 'px';
@@ -95,6 +114,12 @@ window.map = (function () {
     document.addEventListener('mouseup', mouseUpHandler);
   };
 
+  /**
+   * Сброс положения поинтера и возвращения его в центр при деактивации всей
+   * страницы
+   *
+   * @return {undefined}
+   */
   var resetPointerPosition = function () {
     pointer.style.left = pointerDefaultLeft + 'px';
     pointer.style.top = pointerDefaultTop + 'px';
@@ -102,6 +127,13 @@ window.map = (function () {
     updateAddress();
   };
 
+  /**
+   * Обновление пинов на карте.
+   *
+   * @param {array} pinList — список пинов, полученный с сервера через
+   *                          window.data.load()
+   * @return {undefined}
+   */
   var updatePins = function (pinList) {
     window.util.removeElements('.map__pin:not(.map__pin--main)', pinsHolder);
 
@@ -111,20 +143,32 @@ window.map = (function () {
         return;
       }
       var pin = new Pin(pinData);
-      newPins.appendChild(pin.element);
+      if (pin.element) {
+        newPins.appendChild(pin.element);
+      }
     });
     pinsHolder.appendChild(newPins);
     window.card.close();
   };
 
+  /**
+   * Конструктор объекта пина
+   *
+   * @constructor
+   * @param {object} data — объект с данными пина, полученный через загрузку
+   *                        данных с помощью window.data.load()
+   */
   var Pin = function (data) {
+    if (!data.offer) {
+      return;
+    }
     var template = document.querySelector('#pin').content.querySelector('.map__pin');
-
     this.element = template.cloneNode(true);
     this.element.style.left = data.location.x - window.page.PinSize.WIDTH / 2 + 'px';
     this.element.style.top = data.location.y - window.page.PinSize.HEIGHT + 'px';
-    this.element.addEventListener('click', function () {
+    this.element.addEventListener('click', function (evt) {
       window.card.open(data);
+      evt.currentTarget.classList.add('map__pin--active');
     });
 
     var image = this.element.querySelector('img');
@@ -134,21 +178,29 @@ window.map = (function () {
 
   pointer.addEventListener('mousedown', updatePointerPosition);
 
+  // Обработка простого клика на поинтер
   pointer.addEventListener('click', function (evt) {
     if (!window.util.isMouseLeftPressed(evt)) {
       return;
     }
-
     if (!window.page.isActive()) {
       window.page.activate();
     }
     updateAddress();
   });
 
+  // Добавление процедуры деактивации карты при деактивации страницы
   window.page.addDeactivationProcedure(function () {
+    window.util.removeElements('.map__pin:not(.map__pin--main)', pinsHolder);
     setTimeout(resetPointerPosition, 1);
   });
-  window.data.load(updatePins, window.page.showError);
-  window.filter.addPinHandler(window.util.debounce(updatePins));
-  window.page.deactivate();
+  // Подгрузка пинов при активации страницы
+  window.page.addActivationProcedure(function () {
+    window.data.load(updatePins, window.page.showError);
+  });
+//  window.filter.addPinHandler(window.util.debounce(updatePins));
+  
+  return {
+    updatePins: window.util.debounce(updatePins)
+  }
 })();
